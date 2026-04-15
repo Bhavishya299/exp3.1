844d7ff904a574cc5abdcb8c62fc695b422f841b
Bhavishya Chhabra <bhavishyachhabra99@gmail.com>
2026-04-15 14:28:36 +0530

    final project full

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use("/api", require("./routes/protected"));
 
 // MongoDB Connection
 mongoose
-    .connect("mongodb://127.0.0.1:27017/authDB")
+connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB Connected");
         createTestUser(); // 👈 Create user after DB connects
@@ -51,6 +51,8 @@ const createTestUser = async() => {
 };
 
 // Start Server
-app.listen(5000, () => {
-    console.log("🚀 Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
